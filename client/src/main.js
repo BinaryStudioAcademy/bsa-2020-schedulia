608c0df71ff2385a3d62d47b5364224709838ead
lenchvolodymyr <volodymyr.lench@binary-studio.com>
2020-08-03 09:44:18 +0300

    fix style

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import {initializeSentry} from "./services/sentry";
+import { initializeSentry } from './services/sentry';
 
 initializeSentry();
 Vue.config.productionTip = false;
