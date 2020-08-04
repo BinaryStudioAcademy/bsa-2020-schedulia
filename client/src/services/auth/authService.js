a5263d417ee6e00533ddf37a26ff15f135a4db2e
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-04 21:07:36 +0300

    Add methods for token operating

--- client/src/services/auth/authService.js
+++ client/src/services/auth/authService.js
@@ -2,16 +2,40 @@ import requestService from '@/services/requestService';
 
 const authService = {
     async signIn(userLoginData) {
-        return await requestService.post('/auth/login', userLoginData);
+        try {
+            const response = await requestService.post(
+                '/auth/login',
+                userLoginData
+            );
+            this.saveToken(response.access_token);
+            return response;
+        } catch (error) {
+            return Promise.reject(error);
+        }
     },
     async signUp(userRegisterData) {
         return await requestService.post('/auth/register', userRegisterData);
     },
     async signOut() {
-        return await requestService.post('/auth/logout');
+        try {
+            const response = await requestService.post('/auth/logout');
+            this.removeToken();
+            return response;
+        } catch (error) {
+            return Promise.reject(error);
+        }
     },
     async fetchLoggedUser() {
-        return await requestService.get('/auth/me');
+        return await requestService.get('/users/me');
+    },
+    saveToken(token) {
+        localStorage.setItem('auth.accessToken', token);
+    },
+    removeToken() {
+        localStorage.setItem('auth.accessToken', '');
+    },
+    getToken() {
+        return localStorage.getItem('auth.accessToken');
     }
 };
 
