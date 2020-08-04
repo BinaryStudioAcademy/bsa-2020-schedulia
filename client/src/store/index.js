b17da7e6b6f3eb1e55cc7657bfe650c6228c3633
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-04 19:00:51 +0300

    Add module 'auth' to the store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import status from './modules/status';
+import auth from './modules/auth';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: { status },
+    modules: { status, auth },
     plugins: []
 });
