7624124fbcd1437d27104af24eb97a4789d8c1b9
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-04 10:50:34 +0300

    Fix spaces

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,8 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-
 Route::get('/debug-sentry', function () {
     throw new Exception('My first Sentry error!');
 });
