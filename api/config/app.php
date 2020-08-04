af2ff15c4da6ec03db4a0baecd2a62aa23af4093
Andrey <univer1857@gmail.com>
2020-08-04 18:35:55 +0300

    add jwt variable to env.examle, add a line after a package in config/app

--- api/config/app.php
+++ api/config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
