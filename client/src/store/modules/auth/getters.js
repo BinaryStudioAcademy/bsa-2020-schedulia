016509499aca2861a85bda3c663086d9cdfd2c5e
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-04 19:07:09 +0300

    Fix converting to Boolean

--- client/src/store/modules/auth/getters.js
+++ client/src/store/modules/auth/getters.js
@@ -1,8 +1,8 @@
 import * as getters from '@/store/modules/auth/types/getters';
 
 export default {
-    [getters.HAS_TOKEN]: state => Boolean(state.token),
+    [getters.HAS_TOKEN]: state => !!state.token,
     [getters.GET_TOKEN]: state => state.token,
-    [getters.IS_LOGGED_IN]: state => Boolean(state.isLoggedIn),
+    [getters.IS_LOGGED_IN]: state => !!state.isLoggedIn,
     [getters.GET_LOGGED_USER]: state => state.user
 };
