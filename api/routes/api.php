a46f864f183e5e6d84d02037a10d0d11d0542dab
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-04 17:16:27 +0300

    Create routes to register

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['prefix' => 'auth', 'namespace' => 'Api\\Auth'], function () {
+    Route::post('/register', 'AuthController@register');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
