441265cbdfc2a56f652445791f01ead63ecf1cb2
Andrey <univer1857@gmail.com>
2020-08-05 08:41:52 +0300

    Add api routes for login and logout

--- api/routes/api.php
+++ api/routes/api.php
@@ -23,3 +23,10 @@ Route::get('/debug-sentry', function () {
 });
 
 Route::get('/status/{serviceName?}', 'Api\StatusController@status');
+
+Route::prefix('v1')->group(function () {
+    Route::group(['prefix' => 'auth', 'namespace' => 'Api\\Auth'], function () {
+        Route::post('/login', 'AuthController@login');
+        Route::post('/logout', 'AuthController@logout');
+    });
+});
