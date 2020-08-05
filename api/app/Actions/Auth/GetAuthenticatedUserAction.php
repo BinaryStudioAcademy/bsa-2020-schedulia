5d8b55d57071d2a3a396f7e322a428888852e5d4
Andrey <univer1857@gmail.com>
2020-08-05 21:18:26 +0300

    add route and action for auth user profile info

---
+++ api/app/Actions/Auth/GetAuthenticatedUserAction.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Actions\Auth;
+
+
+use Illuminate\Support\Facades\Auth;
+
+class GetAuthenticatedUserAction
+{
+    public function execute(): GetAuthenticatedUserResponse
+    {
+        return new GetAuthenticatedUserResponse(Auth::user());
+    }
+}
