0066d7280e1c26d3cacdc4936b4ecdeb6ff916de
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-05 13:36:53 +0300

    Fix action dispatch

--- client/src/components/guard/UserDataProvider.vue
+++ client/src/components/guard/UserDataProvider.vue
@@ -7,13 +7,14 @@
 <script>
 import authService from '@/services/auth/authService';
 import store from '@/store';
+import * as actions from '@/store/modules/auth/types/actions';
 
 export default {
     name: 'UserDataProvider',
     async beforeRouteEnter(to, from, next) {
         if (!store.state.auth.user && authService.getToken()) {
             try {
-                await store.dispatch('auth/fetchLoggedUser');
+                await store.dispatch('auth/' + actions.FETCH_LOGGED_USER);
                 next({ path: to });
             } catch (error) {
                 console.log(error.message);
