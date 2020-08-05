19f3c7722ef8ca617c0e5b9d225a3314ae4abd35
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-05 12:06:34 +0300

    Create auth controller method register

---
+++ api/app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Actions\Auth\RegisterAction;
+use App\Actions\Auth\RegisterRequest;
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Presenters\RegisterResponseArrayPresenter;
+use App\Http\Requests\Auth\RegisterHttpRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends ApiController
+{
+    private RegisterAction $registerAction;
+    private $presenter;
+
+    public function __construct(
+        RegisterAction $registerAction,
+        RegisterResponseArrayPresenter $registerResponseArrayPresenter
+    ) {
+        $this->registerAction = $registerAction;
+        $this->presenter = $registerResponseArrayPresenter;
+    }
+
+    public function register(RegisterHttpRequest $request)
+    {
+        $response = $this->registerAction->execute(
+            new RegisterRequest(
+                $request->get('email'),
+                $request->get('password'),
+                $request->get('name'),
+//                $request->get('timezone'),
+            )
+        )->getUser();
+
+        return $this->successResponse($this->presenter->present($response));
+    }
+}
