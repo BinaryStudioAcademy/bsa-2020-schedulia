d193665609b3bea13622759cbf3773cd2bd6ddce
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-05 17:10:51 +0300

    Create repository

---
+++ api/app/Repositories/JWTRefreshToken/JWTRefreshTokenRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\JWTRefreshToken;
+
+use App\Entity\JWTRefreshToken;
+use App\Repositories\BaseRepository;
+
+final class JWTRefreshTokenRepository extends BaseRepository implements JWTRefreshTokenRepositoryInterface
+{
+    public function getById(int $id): ?JWTRefreshToken
+    {
+        return JWTRefreshToken::find($id);
+    }
+
+    public function getByUserId(int $userId): ?JWTRefreshToken
+    {
+        return JWTRefreshToken::firstWhere('user_id', $userId);
+    }
+
+    public function save(JWTRefreshToken $JWTRefreshToken): JWTRefreshToken
+    {
+        $JWTRefreshToken->save();
+
+        return $JWTRefreshToken;
+    }
+
+    public function deleteById(int $id): void
+    {
+        JWTRefreshToken::destroy($id);
+    }
+}
---
+++ api/app/Repositories/JWTRefreshToken/JWTRefreshTokenRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\JWTRefreshToken;
+
+use App\Entity\JWTRefreshToken;
+
+interface JWTRefreshTokenRepositoryInterface
+{
+    public function getById(int $id): ?JWTRefreshToken;
+    public function getByUserId(int $userId): ?JWTRefreshToken;
+    public function save(JWTRefreshToken $JWTRefreshToken): JWTRefreshToken;
+    public function deleteById(int $id): void;
+}
