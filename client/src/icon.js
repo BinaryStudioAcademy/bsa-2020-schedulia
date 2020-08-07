39d41c5fb6a836412fe0b3efa35fc65f04d46577
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-07 15:07:14 +0300

    Delete fontawesom icon pack

--- client/src/icon.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faCaretDown);
-Vue.component('FaIcon', FontAwesomeIcon);
--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import './icon.js';
 import { initializeSentry } from './services/sentry';
 
 initializeSentry();
