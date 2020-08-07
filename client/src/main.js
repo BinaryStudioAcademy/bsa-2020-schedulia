494b34eea9e9fbcf7028e063299bc0972a0a5301
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-07 13:34:19 +0300

    Add icon to vue

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import './icon.js';
 import { initializeSentry } from './services/sentry';
 
 initializeSentry();
