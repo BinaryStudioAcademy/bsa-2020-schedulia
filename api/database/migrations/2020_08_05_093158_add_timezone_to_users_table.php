f8e12bfd229decbd8f4ecafc4b49fb82496d7364
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-07 13:51:53 +0300

    Change add timezone migration.

--- api/database/migrations/2020_08_05_093158_add_timezone_to_users_table.php
+++ api/database/migrations/2020_08_05_093158_add_timezone_to_users_table.php
@@ -14,7 +14,7 @@ class AddTimezoneToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('timezone');
+            $table->string('timezone')->default('UTC');
         });
     }
 
@@ -26,7 +26,7 @@ class AddTimezoneToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('timezone')->default('UTC');
+            $table->dropColumn('timezone');
         });
     }
 }
