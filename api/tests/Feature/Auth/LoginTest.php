1d5c0c2fa8864b4201078c7d1ee5599d98d62365
Andrey <univer1857@gmail.com>
2020-08-08 16:57:24 +0300

    Api tests for login

---
+++ api/tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Entity\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\JWTAuth;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_login_with_valid_data_return_token_structure()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make($password = '12345678'),
+            'timezone' => 'Europe/Amsterdam'
+        ]);
+
+        $response = $this->json('POST','/api/v1/auth/login', [
+            'email' => $user->email,
+            'password' => $password
+        ]);
+
+        $response->
+            assertStatus(200)->
+            assertJsonStructure(['data'=>['access_token', 'token_type', 'expires_in']]);
+
+        $responseData =  json_decode($response->getContent(), true);
+
+        $this->assertNotNull(
+            $responseData['data']['access_token']);
+    }
+
+    public function test_login_with_invalid_data_return_error()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make('123456789'),
+            'timezone' => 'Europe/Amsterdam'
+        ]);
+
+        $response = $this->json('POST','/api/v1/auth/login', [
+            'email' => $user->email,
+            'password' => '12345678910'
+        ]);
+
+        $response->assertStatus(401)
+        ->assertJson(['error'=>['message' => 'Unauthenticated.']]);
+    }
+
+    public function test_token_expired()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make($password = '12345678'),
+            'timezone' => 'Europe/Amsterdam'
+        ]);
+
+        $this->expectException(TokenExpiredException::class);
+        auth()->setTTL(0)->attempt(['email' => $user->email, 'password' => $password]);
+
+    }
+}
