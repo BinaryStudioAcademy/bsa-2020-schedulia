c7b73f5240809dc9276e1b6fd19c1e3673d35842
lenchvolodymyr <volodymyr.lench@binary-studio.com>
2020-08-08 14:12:09 +0300

    add header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,65 +1,9 @@
 <template>
-    <VApp>
-        <GeneralLayout />
-        <!--        <VAppBar app color="primary" dark>-->
-        <!--            <div class="d-flex align-center">-->
-        <!--                        <VImg-->
-        <!--                            alt="Vuetify Logo"-->
-        <!--                            class="shrink mr-2"-->
-        <!--                            contain-->
-        <!--                            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
-        <!--                            transition="scale-transition"-->
-        <!--                            width="40"-->
-        <!--                        />-->
-
-        <!--                <VImg-->
-        <!--                    alt="Vuetify Name"-->
-        <!--                    class="shrink mt-1 hidden-sm-and-down"-->
-        <!--                    contain-->
-        <!--                    min-width="100"-->
-        <!--                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
-        <!--                    width="100"-->
-        <!--                />-->
-        <!--            </div>-->
-
-        <!--            <VSpacer></VSpacer>-->
-
-        <!--            <VBtn-->
-        <!--                href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
-        <!--                target="_blank"-->
-        <!--                text-->
-        <!--            >-->
-        <!--                <span class="mr-2">Latest Release</span>-->
-        <!--                <VIcon>mdi-open-in-new</VIcon>-->
-        <!--            </VBtn>-->
-        <!--        </VAppBar>-->
-
-        <VMain>
-            <VContainer>
-                <VRow>
-                    <VCol class="text-center">
-                        <RouterLink to="/">Home</RouterLink>
-                    </VCol>
-                    <VCol class="text-center">
-                        <RouterLink to="/about">About</RouterLink>
-                    </VCol>
-                    <VCol class="text-center">
-                        <RouterLink to="/status">Status</RouterLink>
-                    </VCol>
-                    <VCol class="text-center">
-                        <RouterLink :to="{ name: 'profile' }"
-                            >Profile</RouterLink
-                        >
-                    </VCol>
-                </VRow>
-            </VContainer>
-            <RouterView />
-        </VMain>
-    </VApp>
+    <GeneralLayout />
 </template>
 
 <script>
-import GeneralLayout from './common/GeneralLayout';
+import GeneralLayout from './components/common/GeneralLayout';
 
 export default {
     name: 'App',
---
+++ client/src/components/common/GeneralLayout.vue
@@ -0,0 +1,40 @@
+<template>
+    <VApp>
+        <Header />
+        <!-- <VMain>
+            <VAppBar color="#FFF" absolute app>
+                <VContainer>
+                    <h1>Title</h1>
+                </VContainer>
+            </VAppBar>
+            <VContent style="background-color: #CCC; margin-top: 30px;" app>
+                <VContainer style="background-color: #FFF">
+                    Some text
+                </VContainer>
+            </VContent>
+        </VMain> -->
+    </VApp>
+</template>
+
+<script>
+import Header from './GeneralLayout/Header';
+
+export default {
+    name: 'GeneralLayout',
+
+    components: {
+        Header
+    }
+};
+</script>
+
+<style scoped>
+
+.container h1 {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 36px;
+}
+</style>
