d82bc593216f25edee6214cb1518f8a75d652daa
Andrey <univer1857@gmail.com>
2020-08-08 23:02:24 +0300

    fix test_authenticated_request_return_user_data

--- api/tests/Feature/Auth/GetAuthUserDataTest.php
+++ api/tests/Feature/Auth/GetAuthUserDataTest.php
@@ -14,25 +14,20 @@ class GetAuthUserDataTest extends TestCase
     public function test_authenticated_request_return_user_data()
     {
         $user = factory(User::class)->create([
-            'password' => Hash::make($password = '12345678'),
             'timezone' => 'Europe/Amsterdam'
         ]);
 
-        $this->json('POST', '/api/v1/auth/login', [
-            'email' => $user->email,
-            'password' => $password
-        ]);
-
-        $response = $this->json('GET', '/api/v1/auth/me');
+        $response = $this->withJwt($user)->json('GET', '/api/v1/auth/me');
 
         $response->assertStatus(200)
-            ->assertJsonStructure(['data'=>['email', 'name', 'timezone']]);
-
-        $responseData =  json_decode($response->getContent(), true);
-
-        $this->assertEquals($user->name, $responseData['data']['name']);
-        $this->assertEquals($user->email, $responseData['data']['email']);
-        $this->assertEquals($user->timezone, $responseData['data']['timezone']);
+            ->assertJsonStructure(['data'=>['email', 'name', 'timezone']])
+            ->assertJson([
+                'data' => [
+                    'email' => $user->email,
+                    'name' => $user->name,
+                    'timezone' => $user->timezone,
+                ]
+            ]);
     }
 
     public function test_unauthenticated_request_return_correct_status()
