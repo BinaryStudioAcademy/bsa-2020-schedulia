21661e33623d7931b23dcfa27ad828a92a8dad56
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-09 18:14:32 +0300

    Add registration test with invalid email has been taken

--- api/tests/Feature/Auth/RegistrationTest.php
+++ api/tests/Feature/Auth/RegistrationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Auth;
 
 use App\Entity\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 use Faker\Factory as Faker;
@@ -111,5 +112,30 @@ class RegistrationTest extends TestCase
             'name' => $user['name'],
             'timezone' => $user['timezone'],
         ]);
+    }
+
+    public function test_registration_with_invalid_email_been_taken()
+    {
+        $user = [
+            'email' => 'test@gmail.com',
+            'name' => $this->faker->name,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'timezone' => $this->faker->timezone,
+        ];
+
+        factory(User::class)->create([
+            'name' => $this->faker->name,
+            'email' => 'test@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10),
+        ]);
+
+        $response = $this->json('POST', '/api/v1/auth/register', $user);
+
+        $response->assertStatus(422)
+            ->assertJson(['error'=>['message' => 'The given data was invalid.']])
+            ->assertJson(['error'=>['validator' => ['email' => ['The email has already been taken.']]]]);
     }
 }
