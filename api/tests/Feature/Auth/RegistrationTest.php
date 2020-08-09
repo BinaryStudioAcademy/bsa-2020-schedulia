3395736fc6e797c7dc43a8a6f59f4e603bb539f5
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-09 17:52:09 +0300

    Add registration test with invalid and empty email

--- api/tests/Feature/Auth/RegistrationTest.php
+++ api/tests/Feature/Auth/RegistrationTest.php
@@ -43,4 +43,50 @@ class RegistrationTest extends TestCase
             'timezone' => $user['timezone'],
         ]);
     }
+
+    public function test_registration_with_invalid_email()
+    {
+        $user = [
+            'email' => 'invalid@',
+            'name' => $this->faker->name,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'timezone' => $this->faker->timezone,
+        ];
+
+        $response = $this->json('POST', '/api/v1/auth/register', $user);
+
+        $response->assertStatus(422)
+            ->assertJson(['error'=>['message' => 'The given data was invalid.']])
+            ->assertJson(['error'=>['validator' => ['email' => ['The email must be a valid email address.']]]]);
+
+        $this->assertDatabaseMissing('users', [
+            'email' => $user['email'],
+            'name' => $user['name'],
+            'timezone' => $user['timezone'],
+        ]);
+    }
+
+    public function test_registration_with_empty_email()
+    {
+        $user = [
+            'email' => '',
+            'name' => $this->faker->name,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'timezone' => $this->faker->timezone,
+        ];
+
+        $response = $this->json('POST', '/api/v1/auth/register', $user);
+
+        $response->assertStatus(422)
+            ->assertJson(['error'=>['message' => 'The given data was invalid.']])
+            ->assertJson(['error'=>['validator' => ['email' => ['The email field is required.']]]]);
+
+        $this->assertDatabaseMissing('users', [
+            'email' => $user['email'],
+            'name' => $user['name'],
+            'timezone' => $user['timezone'],
+        ]);
+    }
 }
