a8cce365fc5dc5d6e4524ba6033150105379f3bd
Evgen Zukov <searchyou13@gmail.com>
2020-08-09 12:23:31 +0300

    Task - 97 remove test comments Auth

--- api/tests/Feature/Auth/GetAuthUserDataTest.php
+++ api/tests/Feature/Auth/GetAuthUserDataTest.php
@@ -1,39 +1,39 @@
 <?php
-//
-//namespace Tests\Feature\Auth;
-//
-//use App\Entity\User;
-//use Illuminate\Foundation\Testing\RefreshDatabase;
-//use Illuminate\Support\Facades\Hash;
-//use Tests\TestCase;
-//
-//class GetAuthUserDataTest extends TestCase
-//{
-//    use RefreshDatabase;
-//
-//    public function test_authenticated_request_return_user_data()
-//    {
-//        $user = factory(User::class)->create([
-//            'timezone' => 'Europe/Amsterdam'
-//        ]);
-//
-//        $response = $this->withJwt($user)->json('GET', '/api/v1/auth/me');
-//
-//        $response->assertStatus(200)
-//            ->assertJsonStructure(['data'=>['email', 'name', 'timezone']])
-//            ->assertJson([
-//                'data' => [
-//                    'email' => $user->email,
-//                    'name' => $user->name,
-//                    'timezone' => $user->timezone,
-//                ]
-//            ]);
-//    }
-//
-//    public function test_unauthenticated_request_return_correct_status()
-//    {
-//        $response = $this->json('GET', '/api/v1/auth/me');
-//
-//        $response->assertStatus(401);
-//    }
-//}
+
+namespace Tests\Feature\Auth;
+
+use App\Entity\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class GetAuthUserDataTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_authenticated_request_return_user_data()
+    {
+        $user = factory(User::class)->create([
+            'timezone' => 'Europe/Amsterdam'
+        ]);
+
+        $response = $this->withJwt($user)->json('GET', '/api/v1/auth/me');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data'=>['email', 'name', 'timezone']])
+            ->assertJson([
+                'data' => [
+                    'email' => $user->email,
+                    'name' => $user->name,
+                    'timezone' => $user->timezone,
+                ]
+            ]);
+    }
+
+    public function test_unauthenticated_request_return_correct_status()
+    {
+        $response = $this->json('GET', '/api/v1/auth/me');
+
+        $response->assertStatus(401);
+    }
+}
