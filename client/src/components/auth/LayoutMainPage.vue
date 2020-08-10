21ac8a07a8e2c9b432fc058c194722cd393eb736
Dmitriy Uvin <dimauuvin@gmail.com>
2020-08-10 10:58:02 +0300

    Update LayoutMainPage.vue

--- client/src/components/auth/LayoutMainPage.vue
+++ client/src/components/auth/LayoutMainPage.vue
@@ -1,67 +1,4 @@
 <template>
-    <div>
-        <VRow class="row-height-70">
-            <VCol class="secondary col-2"> </VCol>
-            <VCol class="secondary col-4">
-                <VRow align-="center" justify="start">
-                    <img
-                        :src="require('@/assets/auth/icon_circle_letter_s.svg')"
-                        alt="logo of schedulia"
-                        class="iconCircle"
-                    />
-                    <h2 class="primary--text ml-3 mt-1">
-                        {{ lang.SCHEDULIA }}
-                    </h2>
-                </VRow>
-            </VCol>
-            <VCol class="primary col-6"> </VCol>
-        </VRow>
-        <VRow class="row-height-70">
-            <VCol class="secondary col-6"></VCol>
-            <VCol class="primary col-5"></VCol>
-            <VCol class="primary col-1">
-                <img
-                    :src="require('@/assets/auth/icon_circle_mark_normal.svg')"
-                    alt="icon in circle ok mark normal"
-                    class="iconCircle-59"
-                />
-            </VCol>
-        </VRow>
-        <VRow>
-            <VCol class="secondary col-2"></VCol>
-            <VCol class="secondary col-4">
-                <VRow class="mt-16">
-                    <slot></slot>
-                </VRow>
-            </VCol>
-            <VCol class="primary col-6">
-                <VRow align="start" justify="center">
-                    <Img
-                        :src="require('@/assets/auth/office_worker.svg')"
-                        alt="collage office employee at work"
-                        class="mt-2 officeWorker"
-                    />
-                </VRow>
-            </VCol>
-        </VRow>
-        <VRow class="row-height-200">
-            <VCol class="secondary col-6">
-                <VRow align="start" justify="space-between">
-                    <img
-                        :src="
-                            require('@/assets/auth/icon_circle_mark_rotated.svg')
-                        "
-                        class="mt-n16 ml-16 iconCircle-33"
-                    />
-                    <img
-                        :src="require('@/assets/auth/icon_circle_mail.svg')"
-                        class="mr-n7 mt-8 iconCircle-75"
-                    />
-                </VRow>
-            </VCol>
-            <VCol class="primary col-6"> </VCol>
-        </VRow>
-    </div>
     <VApp>
         <VContainer>
             <VRow no-gutters="true">
@@ -176,15 +113,6 @@ export default {
     margin-top: 61px;
     margin-right: 67px;
 }
-.officeWorker {
-    height: 565px;
-    width: 464px;
-}
-.row-height-70 {
-    height: 70px;
-}
-.row-height-200 {
-    height: 200px;
 
 .office-worker-image {
     height: 464px;
