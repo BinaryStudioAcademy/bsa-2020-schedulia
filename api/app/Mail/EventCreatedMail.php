7889940058b29e3e0429e42a1a7dc5b07767b532
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-11 19:35:00 +0300

    Add EventCreatedMail

---
+++ api/app/Mail/EventCreatedMail.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail;
+
+use App\Entity\Event;
+use App\Entity\EventType;
+use App\Entity\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class EventCreatedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Event $event;
+    public EventType $eventType;
+    public User $owner;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Event $event)
+    {
+        $this->event = $event;
+        $this->eventType = $event->eventType;
+        $this->owner = $event->eventType->owner;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $this->to($this->owner->getEmail());
+
+        return $this
+            ->subject("New Event: {$this->event->invitee_name},
+                    {$this->event->start_date}, {$this->eventType->name}")
+            ->markdown('notifications.event_created', [
+            'event' => $this->event,
+            'eventType' => $this->eventType,
+            'user' => $this->owner
+        ]);
+    }
+}
