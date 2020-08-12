ab9ac079f76d92a3f18eadca74f8993b93399845
Lench Volodymyr <lenchvolodymyr@users.noreply.github.com>
2020-08-12 20:47:49 +0000

    Apply fixes from StyleCI

--- api/app/Events/TestEvent.php
+++ api/app/Events/TestEvent.php
@@ -12,7 +12,9 @@ use Illuminate\Queue\SerializesModels;
 
 class TestEvent implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     public string $message;
 
