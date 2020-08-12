766e058e41e7d48687cdaec3935e031329fd35c4
Dmitriy Uvin <dimauuvin@gmail.com>
2020-08-12 10:29:57 +0300

    Update index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -45,7 +45,7 @@ const routes = [
                         path: 'profile',
                         name: 'Profile',
                         component: () => import('../views/Profile')
-                    },
+                    }
                 ]
             }
         ]
