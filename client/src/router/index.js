e5d906e8f0a3839c7add083c75f1ae8cd8af9269
Andrey <univer1857@gmail.com>
2020-08-12 15:08:21 +0300

    fix style error

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -59,7 +59,7 @@ const routes = [
                         component: () => import('../views/NewEventType')
                     }
                 ]
-            },
+            }
         ]
     }
 ];
