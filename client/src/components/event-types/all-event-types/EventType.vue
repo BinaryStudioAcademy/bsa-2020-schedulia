2c42d565c60caad22d8127426046cc9678984ff3
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-12 23:56:18 +0300

    Add disabled mode

--- client/src/components/event-types/all-event-types/EventType.vue
+++ client/src/components/event-types/all-event-types/EventType.vue
@@ -1,11 +1,12 @@
 <template>
     <div
         class="event-type-block"
-        :style="{ 'border-bottom': '18px solid ' + eventType.color }"
+        :class="{ 'disabled-event': isDisabled }"
+        :style="{ 'border-bottom': '18px solid ' + borderColor }"
     >
         <div class="action-button text-right">
             <VBtn icon color="primary">
-                <VIcon dark>mdi-cog</VIcon>
+                <VIcon dark>mdi-dots-horizontal</VIcon>
             </VBtn>
         </div>
         <div class="event-type-content">
@@ -15,32 +16,33 @@
             </span>
         </div>
         <div class="event-type-invitee mt-9 mb-2">
-            <VAvatar color="teal" size="24">
+            <VAvatar color="teal" size="24" v-if="eventType.invitee">
                 <span class="white--text headline">24</span>
             </VAvatar>
+            <div v-else class="pb-6"></div>
         </div>
         <VDivider />
         <div class="event-type-actions row">
-            <div class="col-3 duration text-left">
-                <div>/{{ eventType.duration }} min</div>
+            <div class="col-lg-3 col-md-3 col-sm-6 duration text-left">
+                <span>/{{ eventType.duration }} min</span>
             </div>
-            <div class="col-md-9 text-right">
+            <div class="col-md-9 col-md-9 col-sm-6 text-right">
                 <VBtn
                     class="ma-2"
                     outlined
-                    :disabled="isDisabled"
                     small
                     :color="eventType.color"
+                    :disabled="isDisabled"
                 >
                     Copy Link
                     <VIcon right dark>mdi-vector-arrange-below </VIcon>
                 </VBtn>
                 <VBtn
                     class="ma-2 text-left"
-                    :disabled="isDisabled"
                     fab
                     small
                     :color="eventType.color"
+                    :disabled="isDisabled"
                 >
                     <VIcon dark>mdi-chevron-down</VIcon>
                 </VBtn>
@@ -60,6 +62,12 @@ export default {
     computed: {
         isDisabled() {
             return this.eventType.disabled;
+        },
+        borderColor() {
+            if (this.isDisabled) {
+                return '#e5e5e5';
+            }
+            return this.eventType.color;
         }
     }
 };
@@ -71,6 +79,9 @@ export default {
     padding: 15px 20px 0 20px;
     border-radius: 10px;
     box-shadow: 0 2px 7px rgba(0, 0, 0, 0.25);
+}
+.event-type-block div {
+    cursor: pointer;
 }
 .event-type-about {
     color: #858585;
@@ -88,14 +99,14 @@ export default {
     font-size: 16px;
 }
 .disabled-event {
-    cursor: default;
+    user-select: none;
 }
 .disabled-event .event-type-content h3,
 .disabled-event .event-type-about,
 .disabled-event .event-type-actions .duration span {
     color: #e5e5e5;
 }
-.disabled-event .event-type-invitee .v-application {
+.disabled-event .event-type-invitee .v-avatar {
     background-color: #e5e5e5 !important;
 }
 </style>
