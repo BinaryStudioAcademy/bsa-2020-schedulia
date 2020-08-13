03b3509a7dcc5b36c60689d689dd192fd4c5aa9b
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-13 16:23:23 +0300

    Complete layout EventTypeList

--- client/src/components/event-types/EventTypesList.vue
+++ client/src/components/event-types/EventTypesList.vue
@@ -14,7 +14,7 @@
         </div>
         <ActionBlock />
         <VDivider />
-        <div class="event-types-block row">
+        <div class="event-types-block row" v-if="eventTypes.length">
             <div
                 class="col-md-3 col-lg-3 col-sm-12"
                 v-for="eventType in eventTypes"
@@ -23,88 +23,23 @@
                 <EventType :eventType="eventType" />
             </div>
         </div>
+        <NoEventTypes v-else />
     </VContainer>
 </template>
 
 <script>
 import ActionBlock from '@/components/event-types/all-event-types/ActionBlock';
 import EventType from '@/components/event-types/all-event-types/EventType';
+import NoEventTypes from '@/components/event-types/all-event-types/NoEventTypes';
 export default {
     name: 'EventTypesList',
     components: {
         ActionBlock,
-        EventType
+        EventType,
+        NoEventTypes
     },
     data: () => ({
-        eventTypes: [
-            {
-                id: 1,
-                name: 'Event Type Name',
-                color: 'green',
-                duration: 45,
-                disabled: false
-            },
-            {
-                id: 2,
-                name: 'Event Type Name 2',
-                color: 'red',
-                duration: 15,
-                invitee: true,
-                disabled: true
-            },
-            {
-                id: 3,
-                name: 'Event Type Name 3',
-                color: 'orange',
-                duration: 54,
-                invitee: true,
-                disabled: false
-            },
-            {
-                id: 4,
-                name: 'Event Type Name 4',
-                color: 'indigo',
-                duration: 54,
-                disabled: true
-            },
-            {
-                id: 5,
-                name: 'Event Type Name 5',
-                color: 'pink',
-                duration: 54,
-                invitee: true,
-                disabled: false
-            },
-            {
-                id: 6,
-                name: 'Event Type Name 6',
-                color: 'red',
-                duration: 15,
-                disabled: false
-            },
-            {
-                id: 7,
-                name: 'Event Type Name 7',
-                color: 'yellow',
-                duration: 15,
-                disabled: false
-            },
-            {
-                id: 8,
-                name: 'Event Type Name 8',
-                color: 'darkorange',
-                duration: 23,
-                invitee: true,
-                disabled: false
-            },
-            {
-                id: 9,
-                name: 'Event Type Name 9',
-                color: '#2be8b4',
-                duration: 89,
-                disabled: false
-            }
-        ]
+        eventTypes: []
     })
 };
 </script>
