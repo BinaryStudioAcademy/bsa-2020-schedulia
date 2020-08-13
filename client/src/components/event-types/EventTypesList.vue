d12307175b44e8914707aac8babfece382eb638c
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-13 11:18:29 +0300

    Add some test data

--- client/src/components/event-types/EventTypesList.vue
+++ client/src/components/event-types/EventTypesList.vue
@@ -16,7 +16,7 @@
         <VDivider />
         <div class="event-types-block row">
             <div
-                class="col-md-3"
+                class="col-md-3 col-lg-3 col-sm-12"
                 v-for="eventType in eventTypes"
                 :key="eventType.id"
             >
@@ -49,7 +49,60 @@ export default {
                 name: 'Event Type Name 2',
                 color: 'red',
                 duration: 15,
+                invitee: true,
                 disabled: true
+            },
+            {
+                id: 3,
+                name: 'Event Type Name 3',
+                color: 'orange',
+                duration: 54,
+                invitee: true,
+                disabled: false
+            },
+            {
+                id: 4,
+                name: 'Event Type Name 4',
+                color: 'indigo',
+                duration: 54,
+                disabled: true
+            },
+            {
+                id: 5,
+                name: 'Event Type Name 5',
+                color: 'pink',
+                duration: 54,
+                invitee: true,
+                disabled: false
+            },
+            {
+                id: 6,
+                name: 'Event Type Name 6',
+                color: 'red',
+                duration: 15,
+                disabled: false
+            },
+            {
+                id: 7,
+                name: 'Event Type Name 7',
+                color: 'yellow',
+                duration: 15,
+                disabled: false
+            },
+            {
+                id: 8,
+                name: 'Event Type Name 8',
+                color: 'darkorange',
+                duration: 23,
+                invitee: true,
+                disabled: false
+            },
+            {
+                id: 9,
+                name: 'Event Type Name 9',
+                color: '#2be8b4',
+                duration: 89,
+                disabled: false
             }
         ]
     })
