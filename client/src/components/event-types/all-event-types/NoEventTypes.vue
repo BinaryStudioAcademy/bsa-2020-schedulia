44d9e6df07cd7b73fb2d02e3d21bf6e061d56bab
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-13 15:37:47 +0300

    Add NoEventTypes component

---
+++ client/src/components/event-types/all-event-types/NoEventTypes.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-center pt-5">
+        <h3>
+            You don't have any event types yet.
+        </h3><br />
+        <p>
+            You'll want to add an event type to allow people to schedule with you.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NoEventTypes'
+};
+</script>
+
+<style scoped>
+
+</style>
