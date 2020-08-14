6ba477bbab67d0a867c7b835032a730fed077616
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-14 10:53:37 +0300

    Add filter on past page

---
+++ client/src/components/event-scheduled/Past/Filter/EnviteeEmails.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="text-left filter-menu">
+        <div class="filter-title">
+            Envitee Emails
+        </div>
+        <VMenu
+            v-model="menu"
+            max-width="290"
+            :close-on-content-click="false"
+            offset-y
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <VBtn
+                    :ripple="false"
+                    :hover="false"
+                    class="filter-menu__button"
+                    text
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                    All Envitee Emails
+                    <VIcon>mdi-chevron-down</VIcon>
+                </VBtn>
+            </template>
+
+            <VCard>
+                <VList>
+                    <VListItem>
+                        <VListItemContent>
+                            <VTextField
+                                solo
+                                hide-details
+                                dense
+                                flat
+                                color="#2C2C2C"
+                                background-color="rgba(224, 224, 224, 0.3)"
+                                label="Search"
+                                prepend-inner-icon="mdi-magnify"
+                            ></VTextField>
+                            <VContainer fluid>
+                                <VCheckbox
+                                    hide-details
+                                    label="15 Minute Meeting"
+                                    value=""
+                                ></VCheckbox>
+                                <VCheckbox
+                                    hide-details
+                                    label="30 Minute Meeting"
+                                    value=""
+                                ></VCheckbox>
+                                <VCheckbox
+                                    hide-details
+                                    label="60 Minute Meeting"
+                                    value=""
+                                ></VCheckbox>
+                                <VCheckbox
+                                    hide-details
+                                    label="Sales Manager"
+                                    value=""
+                                ></VCheckbox>
+                                <VCheckbox
+                                    hide-details
+                                    label="Sales Manager"
+                                    value=""
+                                ></VCheckbox>
+                            </VContainer>
+                            <VContainer class="filter-button">
+                                <VBtn class="cancel-button" outlined>
+                                    Cancel
+                                </VBtn>
+                                <VBtn class="apply-button primary">
+                                    Apply
+                                </VBtn>
+                            </VContainer>
+                        </VListItemContent>
+                    </VListItem>
+                </VList>
+
+                <VDivider></VDivider>
+            </VCard>
+        </VMenu>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EnviteeEmails'
+};
+</script>
---
+++ client/src/components/event-scheduled/Past/Past.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <FilterList />
+        <BorderBottom />
+        <Events />
+        <Events />
+    </div>
+</template>
+
+<script>
+import FilterList from './Filter/FilterList';
+import BorderBottom from '../../common/GeneralLayout/BorderBottom';
+import Events from '../ScheduledEvents/Events';
+export default {
+    name: 'Past',
+
+    components: {
+        Events,
+        BorderBottom,
+        FilterList
+    }
+};
+</script>
+
+<style scoped></style>
