fde79877beadd4314a124cd97a16eaba0b865a60
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-14 23:12:40 +0300

    Some fixes

--- client/src/components/event-types/all-event-types/ActionBlock.vue
+++ client/src/components/event-types/all-event-types/ActionBlock.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="row">
-        <div class="col-md-6 row">
-            <div class="col-md-1 text-right">
+        <div class="row col-md col-sm-12">
+            <div class="col-lg-1 col-md-1 col-sm-2">
                 <Avatar :size="48"></Avatar>
             </div>
 
-            <div class="col">
+            <div class="col-lg-11 col-md-11 col-sm-10">
                 <span class="user-name">{{ user.name }}</span>
                 <br />
                 <a href="">shedulia.xyz/nickname</a>
             </div>
         </div>
-        <div class="col-md-6 text-right new-event-type-btn">
+        <div class="col-md text-right">
             <RouterLink :to="{ name: 'new-event' }">
                 <VBtn class="ma-2" outlined color="indigo">
                     {{ lang.NEW_EVENT_TYPE }}
