af53991d92f50f8734fdfb013c07f3f93bff4e44
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-14 10:52:24 +0300

    Add close and export button to tab row

---
+++ client/src/components/event-scheduled/CloseButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <VBtn class="close-button primary" depressed>
+        <VRow>
+            <VCol class="text-left" align-self="center">
+                Close
+            </VCol>
+            <VSpacer></VSpacer>
+            <VCol class="text-right">
+                <VIcon small>mdi-close</VIcon>
+            </VCol>
+        </VRow>
+    </VBtn>
+</template>
+
+<script>
+export default {
+    name: 'CloseButton'
+};
+</script>
+
+<style scoped>
+.close-button::v-deep {
+    width: 120px;
+    text-transform: none;
+    margin-right: 14px;
+}
+</style>
---
+++ client/src/components/event-scheduled/ExportButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <VBtn class="export-button" outlined>
+        <VRow>
+            <VCol class="text-left" align-self="center">
+                Export
+            </VCol>
+            <VCol class="text-right">
+                <VIcon small>mdi-export</VIcon>
+            </VCol>
+        </VRow>
+    </VBtn>
+</template>
+
+<script>
+export default {
+    name: 'ExportButton'
+};
+</script>
+
+<style scoped>
+.export-button::v-deep {
+    width: 120px;
+    text-transform: none;
+    color: var(--v-primary-base);
+}
+</style>
