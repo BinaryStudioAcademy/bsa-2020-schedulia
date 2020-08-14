736975053a37a0fc4d6707b5128cc0c8419b11a9
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-14 10:58:58 +0300

    Update EventType using repository

--- api/app/Actions/EventType/ChangeDisabledByIdAction.php
+++ api/app/Actions/EventType/ChangeDisabledByIdAction.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace App\Actions\EventType;
 
-use App\Entity\EventType;
+use App\Repositories\EventType\EventTypeRepository;
+use App\Repositories\EventType\EventTypeRepositoryInterface;
 
 final class ChangeDisabledByIdAction
 {
+    private EventTypeRepositoryInterface $repository;
+
+    public function __construct(EventTypeRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function execute(ChangeDisabledByIdRequest $request): void
     {
-        $eventType = EventType::find($request->getId());
+        $eventType = $this->repository->getById($request->getId());
         $eventType->disabled = $request->getDisabled();
-        $eventType->save();
+        $this->repository->save($eventType);
     }
 }
