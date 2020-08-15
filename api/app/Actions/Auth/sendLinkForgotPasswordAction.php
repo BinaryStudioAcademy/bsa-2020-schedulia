5f331ab6cc402de8e4ea554ef058da94e66888d3
mv-v-7 <69146987+mv-v-7@users.noreply.github.com>
2020-08-15 22:46:40 +0300

    custom email link reset

--- api/app/Actions/Auth/sendLinkForgotPasswordAction.php
+++ api/app/Actions/Auth/sendLinkForgotPasswordAction.php
@@ -4,6 +4,9 @@ namespace App\Actions\Auth;
 
 use App\Exceptions\Auth\UserWithThisEmailDoesNotExist;
 use App\Repositories\User\UserRepository;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Password;
 
 final class sendLinkForgotPasswordAction
@@ -20,6 +23,13 @@ final class sendLinkForgotPasswordAction
         {
             throw new UserWithThisEmailDoesNotExist($request->getEmail());
         }
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            $linkReset = url(config('app.url').route('password.reset', ['token' => $token, 'email' => $notifiable->getEmailForPasswordReset()], false));
+            $count = config('auth.passwords.'.config('auth.defaults.passwords').'.expire');
+            return (new MailMessage)
+                ->subject(Lang::get('Reset Password from Schedulia'))
+                ->view('emails.sendLinkResetPassword', ['linkReset' =>$linkReset, 'count'=>$count]);
+        });
         $credentials = ['email' =>$request->getEmail()];
         Password::sendResetLink($credentials);
         return (new sendLinkForgotPasswordResponse( ['email' =>$request->getEmail(), 'status'=>200]));
---
+++ api/resources/views/emails/sendLinkResetPassword.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:100,300,400,500,700,900">
+    <title>Reset password in Schedulia</title>
+
+</head>
+<body>
+<h2>Dear user</h2>
+<p>You are receiving this email because we received a password reset request for your account. Please use link below to change password.</p>
+<a href="{{$linkReset}}" >Reset password</a>
+<p>This password reset link will expire in {{$count}} minutes.</p>
+<p>If you did not request a password reset, no further action is required.</p>
+<p>If link does not work please copy link below and paste in browser
+</p>
+<p>{{$linkReset}}</p>
+<p>Your Schedulia Team</p>
+</body>
+</html>
