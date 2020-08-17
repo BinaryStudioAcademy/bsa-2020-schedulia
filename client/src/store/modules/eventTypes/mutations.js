8275feeb7eb4320b8eadeb3f57be21ebef4f818c
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-17 14:38:41 +0300

    Add mutations for searching to store

--- client/src/store/modules/eventTypes/mutations.js
+++ client/src/store/modules/eventTypes/mutations.js
@@ -20,5 +20,8 @@ export default {
         state.eventTypes = state.eventTypes.filter(eventType => {
             return eventType.id !== id;
         });
+    },
+    [mutations.SEARCHED_EVENT_TYPES]: (state, searchedEventTypes) => {
+        state.eventTypes = searchedEventTypes;
     }
 };
--- client/src/store/modules/eventTypes/types/mutations.js
+++ client/src/store/modules/eventTypes/types/mutations.js
@@ -1,3 +1,4 @@
 export const SET_EVENT_TYPES = 'setEventTypes';
 export const DISABLE_EVENT_TYPE_BY_ID = 'disableEventTypeById';
 export const DELETE_EVENT_TYPE_BY_ID = 'deleteEventTypeById';
+export const SEARCHED_EVENT_TYPES = 'searchedEventTypes';
