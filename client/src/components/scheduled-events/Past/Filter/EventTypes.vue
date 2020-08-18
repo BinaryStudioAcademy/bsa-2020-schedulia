1b414d484f8af282e3c0469606489257b3d68581
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-18 18:44:27 +0300

    Finish filter event types layout

--- client/src/components/scheduled-events/Past/Filter/EventTypes.vue
+++ client/src/components/scheduled-events/Past/Filter/EventTypes.vue
@@ -18,7 +18,12 @@
                     v-bind="attrs"
                     v-on="on"
                 >
-                    All Evebt Types
+                    <span v-if="!eventTypes.length">
+                        All Event Types
+                    </span>
+                    <span v-else>
+                        {{ eventTypes.length }} Event Types
+                    </span>
                     <VIcon>mdi-chevron-down</VIcon>
                 </VBtn>
             </template>
@@ -38,32 +43,80 @@
                                 clearable
                                 prepend-inner-icon="mdi-magnify"
                             ></VTextField>
-                            <VContainer fluid>
-                                <VCheckbox
-                                    hide-details
-                                    label="15 Minute Meeting"
-                                    value=""
-                                ></VCheckbox>
-                                <VCheckbox
-                                    hide-details
-                                    label="30 Minute Meeting"
-                                    value=""
-                                ></VCheckbox>
-                                <VCheckbox
-                                    hide-details
-                                    label="60 Minute Meeting"
-                                    value=""
-                                ></VCheckbox>
-                                <VCheckbox
-                                    hide-details
-                                    label="Sales Manager"
-                                    value=""
-                                ></VCheckbox>
-                                <VCheckbox
-                                    hide-details
-                                    label="Sales Manager"
-                                    value=""
-                                ></VCheckbox>
+                            <VContainer class="filter-form" fluid>
+                                <VBtn
+                                        :ripple="false"
+                                        :hover="false"
+                                        class="filter-form__button"
+                                        text
+                                >
+                                    select all
+                                </VBtn>
+                                \
+                                <VBtn
+                                        :ripple="false"
+                                        :hover="false"
+                                        class="filter-form__button"
+                                        text
+                                >
+                                    clear
+                                </VBtn>
+                                <div class="filter-form__checkbox">
+                                    <VCheckbox
+                                        hide-details
+                                        label="15 Minute Meeting"
+                                        v-model="eventTypes"
+                                        value="1"
+                                    ></VCheckbox>
+                                    <VCheckbox
+                                        hide-details
+                                        label="30 Minute Meeting"
+                                        v-model="eventTypes"
+                                        value="2"
+                                    ></VCheckbox>
+                                    <VCheckbox
+                                        hide-details
+                                        label="60 Minute Meeting"
+                                        v-model="eventTypes"
+                                        value="3"
+                                    ></VCheckbox>
+                                    <VCheckbox
+                                        hide-details
+                                        label="Day meeting"
+                                        v-model="eventTypes"
+                                        value="4"
+                                    ></VCheckbox>
+                                    <VCheckbox
+                                        hide-details
+                                        label="Skype call"
+                                        v-model="eventTypes"
+                                        value="5"
+                                    ></VCheckbox>
+                                    <VBtn
+                                            :ripple="false"
+                                            :hover="false"
+                                            class="filter-form__button more"
+                                            text
+                                            v-if="!moreEventTypes"
+                                            @click="showMoreEventTypes"
+                                    >
+                                        Show more
+                                    </VBtn>
+                                    <div v-else>
+                                        <VCheckbox
+                                                hide-details
+                                                label="Viber call"
+                                                v-model="eventTypes"
+                                                value="6"
+                                        ></VCheckbox>
+                                        <VCheckbox
+                                                hide-details
+                                                label="Phone call"
+                                                v-model="eventTypes"
+                                                value="7"
+                                        ></VCheckbox>
+                                    </div>
+                                </div>
                             </VContainer>
                             <VContainer class="filter-button">
                                 <VBtn
@@ -93,13 +146,23 @@ export default {
 
     data() {
         return {
-            menu: false
+            menu: false,
+            eventTypes: ["1"],
+            moreEventTypes: false
         };
     },
 
     methods: {
         closeMenu() {
             this.menu = false;
+        },
+
+        showMoreEventTypes() {
+            this.moreEventTypes = true;
+        },
+
+        selectedEventTypes() {
+            return this.eventTypes.length;
         }
     }
 };
