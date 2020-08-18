17652802cd7db3f8a9ab2900b98c9e533ae47665
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-18 21:41:11 +0300

    Add methods depending on date

--- api/app/Services/Availability/AvailabilityTypes.php
+++ api/app/Services/Availability/AvailabilityTypes.php
@@ -29,4 +29,44 @@ final class AvailabilityTypes
     public const WEEKEND_PRIORITY = 3;
     public const EXACT_DATE_PRIORITY = 1;
     public const EVERY_DAY_PRIORITY = 4;
+
+    public static function getTypes(): array
+    {
+        return [
+            self::EVERY_MONDAY,
+            self::EVERY_TUESDAY,
+            self::EVERY_WEDNESDAY,
+            self::EVERY_THURSDAY,
+            self::EVERY_FRIDAY,
+            self::EVERY_SATURDAY,
+            self::EVERY_SUNDAY,
+            self::WEEKDAYS,
+            self::WEEKEND,
+            self::EXACT_DATE,
+            self::EVERY_DAY,
+        ];
+    }
+
+    public static function getTypesInDependsOnDate(): array
+    {
+        return [
+            self::EVERY_MONDAY,
+            self::EVERY_TUESDAY,
+            self::EVERY_WEDNESDAY,
+            self::EVERY_THURSDAY,
+            self::EVERY_FRIDAY,
+            self::EVERY_SATURDAY,
+            self::EVERY_SUNDAY,
+            self::WEEKDAYS,
+            self::WEEKEND,
+            self::EVERY_DAY,
+        ];
+    }
+
+    public static function getTypesDependsOnDate(): array
+    {
+        return [
+            self::EXACT_DATE,
+        ];
+    }
 }
