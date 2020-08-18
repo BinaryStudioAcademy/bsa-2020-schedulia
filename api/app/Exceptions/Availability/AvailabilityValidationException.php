fc390f2675b209655f599788b1207d9282049880
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-18 21:12:25 +0300

    Add custom Exception for Availability

---
+++ api/app/Exceptions/Availability/AvailabilityValidationException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions\Availability;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+final class AvailabilityValidationException extends HttpException
+{
+}
