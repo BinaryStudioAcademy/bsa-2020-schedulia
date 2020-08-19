00c2c4115a9424f39d92f95c4966f46306579f55
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-19 13:30:19 +0300

    Fix validation for situation when end_date is midnight

--- api/app/Services/Availability/AvailabilityService.php
+++ api/app/Services/Availability/AvailabilityService.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Collection;
 
 final class AvailabilityService implements AvailabilityServiceInterface
 {
+    private const MIDNIGHT_TIME = "00:00:00";
     public function validateAvailabilities(Collection $availabilities, int $duration)
     {
         foreach ($availabilities as $availability) {
@@ -23,7 +24,12 @@ final class AvailabilityService implements AvailabilityServiceInterface
 
     private function validateAvailability(Availability $availability, int $duration)
     {
-        if ($availability->start_date > $availability->end_date) {
+        $startDate = explode(" ", $availability->start_date)[0];
+        $endDate = explode(" ", $availability->end_date)[0];
+        $endTime = explode(" ", $availability->end_date)[1];
+
+        if ($availability->start_date > $availability->end_date && $endTime !== self::MIDNIGHT_TIME) {
+
             throw new AvailabilityValidationException(400, "Your end time cannot be before your start time!");
         } else {
             $startDateWithDuration = new Carbon($availability->start_date);
@@ -37,10 +43,7 @@ final class AvailabilityService implements AvailabilityServiceInterface
         if (!in_array($availability->type, AvailabilityTypes::getTypes())) {
             throw new AvailabilityValidationException(400, "Unknown Availability type!");
         } else {
-            $startDate = explode(" ", $availability->start_date)[0];
-            $endDate = explode(" ", $availability->end_date)[0];
-            $endTime = explode(" ", $availability->end_date)[1];
-            if ($startDate !== $endDate && $endTime !== '00:00:00') {
+            if ($startDate !== $endDate && $endTime !== self::MIDNIGHT_TIME) {
                 throw new AvailabilityValidationException(400, "Date for Availability with type '{$availability->type}' must be from one day!");
             }
         }
