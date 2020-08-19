016dd5db12744ec9896d190ae3444ad1122de5be
Mike Maschin <aussie.m.mike@gmail.com>
2020-08-19 13:00:55 +0300

    add nickname

--- api/app/Http/Requests/Api/User/UpdateUserRequest.php
+++ api/app/Http/Requests/Api/User/UpdateUserRequest.php
@@ -18,6 +18,7 @@ final class UpdateUserRequest extends ApiFormRequest
             'time_format_12h' => 'boolean',
             'country' => 'nullable|string',
             'timezone' => 'nullable|string',
+            'nickname' => 'nullable|string|unique:users,nickname',
         ];
     }
 }
