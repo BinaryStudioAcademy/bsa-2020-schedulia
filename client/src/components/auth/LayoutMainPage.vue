f86508ef46a37e4f3f7828783abc6cae80b294e0
mv-v-7 <69146987+mv-v-7@users.noreply.github.com>
2020-08-21 08:46:55 +0300

    vuelidate and logical fix in SignIn

--- client/src/components/auth/LayoutMainPage.vue
+++ client/src/components/auth/LayoutMainPage.vue
@@ -1,99 +1,97 @@
 <template>
-    <VApp>
-        <VContainer fluid pa-0>
-            <VRow no-gutters>
-                <VCol class="primary-vcol" xl="6" lg="6" md="6" cols="12">
-                    <div class="logo-primary-div">
-                        <RouterLink :to="{ name: 'SignIn' }">
-                            <VRow no-gutters align="center" justify="center">
-                                <img
-                                    :src="
-                                        require('@/assets/auth/icon_circle_letter_s.svg')
-                                    "
-                                    alt="logo of schedulia"
-                                    class="logo-image"
-                                />
-                                <h5 class="logo-h5 secondary--text">
-                                    {{ lang.SCHEDULIA }}
-                                </h5>
-                            </VRow>
-                        </RouterLink>
-                    </div>
-                    <VRow no-gutters justify="end">
-                        <img
-                            :src="
-                                require('@/assets/auth/icon_circle_mark_normal.svg')
-                            "
-                            alt="icon in circle ok mark normal"
-                            class="normal-ok-image "
-                        />
-                    </VRow>
-                    <VRow no-gutters justify="center" align="end">
-                        <Img
-                            :src="require('@/assets/auth/office_worker.png')"
-                            alt="collage office employee at work"
-                            class="office-worker-image"
-                        />
-                    </VRow>
-                    <VRow no-gutters justify="start">
-                        <img
-                            :src="require('@/assets/auth/icon_circle_mail.svg')"
-                            alt="envelop sign"
-                            class="envelop-image"
-                        />
-                    </VRow>
-                </VCol>
-                <VCol class="secondary-vcol " xl="6" lg="6" md="6" cols="12">
-                    <VRow no-gutters justify="end">
-                        <VCol
-                            class="first-column-in-secondary"
-                            xl="4"
-                            lg="4"
-                            md="4"
-                            cols="1"
-                        >
+    <VContainer fluid pa-0>
+        <VRow no-gutters>
+            <VCol class="primary-vcol" xl="6" lg="6" md="6" cols="12">
+                <div class="logo-primary-div">
+                    <RouterLink :to="{ name: 'SignIn' }">
+                        <VRow no-gutters align="center" justify="center">
                             <img
                                 :src="
-                                    require('@/assets/auth/icon_circle_mark_rotated.svg')
+                                    require('@/assets/auth/icon_circle_letter_s.svg')
                                 "
-                                alt="rotated ok sign"
-                                class="rotated-ok-image "
+                                alt="logo of schedulia"
+                                class="logo-image"
                             />
-                        </VCol>
-                        <VCol
-                            class="second-column-in-secondary"
-                            xl="8"
-                            lg="8"
-                            md="8"
-                            cols="11"
-                        >
-                            <div class="logo-secondary-div ">
-                                <RouterLink :to="{ name: 'SignIn' }">
-                                    <VRow no-gutters align="center">
-                                        <img
-                                            :src="
-                                                require('@/assets/auth/icon_circle_letter_s.svg')
-                                            "
-                                            alt="logo of schedulia"
-                                            class="logo-image"
-                                        />
-                                        <h5 class="logo-h5 ">
-                                            {{ lang.SCHEDULIA }}
-                                        </h5>
-                                    </VRow>
-                                </RouterLink>
-                            </div>
-                            <VRow no-gutters justify="start" align="start">
-                                <VCol class="place-for-form-vcol">
-                                    <slot></slot>
-                                </VCol>
-                            </VRow>
-                        </VCol>
-                    </VRow>
-                </VCol>
-            </VRow>
-        </VContainer>
-    </VApp>
+                            <h5 class="logo-h5 secondary--text">
+                                {{ lang.SCHEDULIA }}
+                            </h5>
+                        </VRow>
+                    </RouterLink>
+                </div>
+                <VRow no-gutters justify="end">
+                    <img
+                        :src="
+                            require('@/assets/auth/icon_circle_mark_normal.svg')
+                        "
+                        alt="icon in circle ok mark normal"
+                        class="normal-ok-image "
+                    />
+                </VRow>
+                <VRow no-gutters justify="center" align="end">
+                    <Img
+                        :src="require('@/assets/auth/office_worker.png')"
+                        alt="collage office employee at work"
+                        class="office-worker-image"
+                    />
+                </VRow>
+                <VRow no-gutters justify="start">
+                    <img
+                        :src="require('@/assets/auth/icon_circle_mail.svg')"
+                        alt="envelop sign"
+                        class="envelop-image"
+                    />
+                </VRow>
+            </VCol>
+            <VCol class="secondary-vcol " xl="6" lg="6" md="6" cols="12">
+                <VRow no-gutters justify="end">
+                    <VCol
+                        class="first-column-in-secondary"
+                        xl="4"
+                        lg="4"
+                        md="4"
+                        cols="1"
+                    >
+                        <img
+                            :src="
+                                require('@/assets/auth/icon_circle_mark_rotated.svg')
+                            "
+                            alt="rotated ok sign"
+                            class="rotated-ok-image "
+                        />
+                    </VCol>
+                    <VCol
+                        class="second-column-in-secondary"
+                        xl="8"
+                        lg="8"
+                        md="8"
+                        cols="11"
+                    >
+                        <div class="logo-secondary-div ">
+                            <RouterLink :to="{ name: 'SignIn' }">
+                                <VRow no-gutters align="center">
+                                    <img
+                                        :src="
+                                            require('@/assets/auth/icon_circle_letter_s.svg')
+                                        "
+                                        alt="logo of schedulia"
+                                        class="logo-image"
+                                    />
+                                    <h5 class="logo-h5 ">
+                                        {{ lang.SCHEDULIA }}
+                                    </h5>
+                                </VRow>
+                            </RouterLink>
+                        </div>
+                        <VRow no-gutters justify="start" align="start">
+                            <VCol class="place-for-form-vcol">
+                                <slot></slot>
+                            </VCol>
+                        </VRow>
+                    </VCol>
+                </VRow>
+            </VCol>
+        </VRow>
+    </VContainer>
 </template>
 
 <script>
