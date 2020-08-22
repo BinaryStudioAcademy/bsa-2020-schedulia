9de2778b4ce99f1509e60d2e39db52aa0d8238cc
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-22 16:21:09 +0300

    Add new component avatar and refactor old code where avatar used

---
+++ client/src/components/common/Avatar/Avatar.vue
@@ -0,0 +1,44 @@
+<template>
+    <VAvatar :size="avatarSize">
+        <VImg v-if="avatar" :src="avatar"></VImg>
+        <VIcon v-else :size="size" :color="color">
+            mdi-account-circle
+        </VIcon>
+    </VAvatar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import * as getters from '@/store/modules/auth/types/getters';
+export default {
+    name: 'Avatar',
+    props: {
+        disabled: {
+            required: false
+        },
+        size: {
+            required: false,
+            default: 36
+        },
+        color: {
+            required: false,
+            default: 'white'
+        }
+    },
+    computed: {
+        ...mapGetters('auth', {
+            user: getters.GET_LOGGED_USER
+        }),
+
+        avatar() {
+            return this.user.avatar;
+        }
+    }
+};
+</script>
+
+<style scoped>
+.v-avatar::v-deep {
+    margin-right: 15px;
+}
+</style>
