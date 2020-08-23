a9b6cf5770417282966f0c6b65a576ebbd7a1f73
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-23 20:52:25 +0300

    Add guard for pages that are not available for authorized

---
+++ client/src/components/guard/AuthGuard.vue
@@ -0,0 +1,20 @@
+<template>
+    <RouterView></RouterView>
+</template>
+
+<script>
+    import store from '@/store';
+
+    export default {
+        name: 'AuthGuard',
+        beforeRouteEnter(to, from, next) {
+            if (!store.state.auth.user) {
+                next();
+            } else {
+                next({ path: '/' });
+            }
+        }
+    };
+</script>
+
+<style scoped></style>
