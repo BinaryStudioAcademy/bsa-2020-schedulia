5823c8343e721e60e1554a3d632a665167fb1e84
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-23 19:01:44 +0300

    Add Loader to the app

--- client/src/store/modules/eventTypes/actions.js
+++ client/src/store/modules/eventTypes/actions.js
@@ -2,20 +2,28 @@ import eventTypesService from '@/services/event-types/eventTypesService';
 import * as actions from './types/actions';
 import * as mutations from './types/mutations';
 import * as authActions from '@/store/modules/auth/types/actions';
+import * as loaderMutations from '@/store/modules/loader/types/mutations';
 
 export default {
     [actions.FETCH_EVENT_TYPES]: async (
         { commit, dispatch },
         searchString = ''
     ) => {
+        commit('loader/' + loaderMutations.SET_LOADING, true, { root: true });
         try {
             const eventTypes = await eventTypesService.fetchAllEventTypes(
                 searchString
             );
             commit(mutations.SET_EVENT_TYPES, eventTypes);
+            commit('loader/' + loaderMutations.SET_LOADING, false, {
+                root: true
+            });
             return eventTypes;
         } catch (error) {
             dispatch('auth/' + authActions.CHECK_IF_UNAUTHORIZED, error, {
+                root: true
+            });
+            commit('loader/' + loaderMutations.SET_LOADING, false, {
                 root: true
             });
         }
@@ -24,6 +32,7 @@ export default {
         { commit, dispatch },
         { id, disabled }
     ) => {
+        commit('loader/' + loaderMutations.SET_LOADING, true, { root: true });
         try {
             await eventTypesService.changeDisabledEventTypeById({
                 id,
@@ -33,23 +42,28 @@ export default {
                 id,
                 disabled
             });
+            commit('loader/' + loaderMutations.SET_LOADING, false, { root: true });
         } catch (error) {
             dispatch('auth/' + authActions.CHECK_IF_UNAUTHORIZED, error, {
                 root: true
             });
+            commit('loader/' + loaderMutations.SET_LOADING, false, { root: true });
         }
     },
     [actions.DELETE_EVENT_TYPE_BY_ID]: async (
         { commit, dispatch },
         eventTypeId
     ) => {
+        commit('loader/' + loaderMutations.SET_LOADING, true, { root: true });
         try {
             await eventTypesService.deleteEventTypeById(eventTypeId);
             commit(mutations.DELETE_EVENT_TYPE_BY_ID, eventTypeId);
+            commit('loader/' + loaderMutations.SET_LOADING, false, { root: true });
         } catch (error) {
             dispatch('auth/' + authActions.CHECK_IF_UNAUTHORIZED, error, {
                 root: true
             });
+            commit('loader/' + loaderMutations.SET_LOADING, false, { root: true });
         }
     }
 };
