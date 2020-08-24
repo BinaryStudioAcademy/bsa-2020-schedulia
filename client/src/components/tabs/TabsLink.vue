939ce44cb25432654d5bef0dc2c3eb21d8d781d9
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-24 23:58:45 +0300

    Add new component: tabs link

---
+++ client/src/components/tabs/TabsLink.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="tabs">
+        <div class="tabs__list">
+            <VContainer class="tabs__container">
+                <VToolbar color="white" dense flat>
+                    <VRow align="end">
+                        <VCol class="text-left">
+                            <VTabs v-model="tab">
+                                <VTab v-for="item in tabs" :key="item.tab" :to="item.route">
+                                    {{ item.title }}
+                                </VTab>
+                            </VTabs>
+                        </VCol>
+                        <VCol class="text-right" align-self="center">
+                            <slot name="right-side"></slot>
+                        </VCol>
+                    </VRow>
+                </VToolbar>
+            </VContainer>
+        </div>
+        <BorderBottom />
+    </div>
+</template>
+
+<script>
+    import BorderBottom from '../common/GeneralLayout/BorderBottom';
+
+    export default {
+        name: 'Tabs',
+        components: {
+            BorderBottom
+        },
+        props: {
+            tabs: {
+                type: Array,
+                required: true
+            }
+        },
+        data: () => ({
+            tab: null
+        })
+    };
+</script>
+
+<style lang="scss" scoped>
+    .tabs {
+        &__list {
+            width: 100%;
+            background: var(--v-background-lighten1);
+        }
+
+        &__container {
+            padding: 0;
+        }
+    }
+    .v-tab {
+        text-transform: none;
+    }
+</style>
