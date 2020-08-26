e8a9d7813566ecf0cf846c091f55e28c39aeeff1
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-26 10:14:14 +0300

    Fix repository contract

--- api/app/Repositories/User/UserRepositoryInterface.php
+++ api/app/Repositories/User/UserRepositoryInterface.php
@@ -6,7 +6,6 @@ namespace App\Repositories\User;
 
 use App\Contracts\EloquentCriterion;
 use App\Entity\User;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
 interface UserRepositoryInterface
@@ -16,5 +15,5 @@ interface UserRepositoryInterface
     public function save(User $user): User;
     public function deleteById(int $id): void;
     public function findByCriteria(EloquentCriterion ...$criteria): Collection;
-    public function findOneByCriteria(EloquentCriterion ...$criteria): Model;
+    public function findOneByCriteria(EloquentCriterion ...$criteria): User;
 }
