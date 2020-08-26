286ee2816464aa7a1598eea12842f89cc85646f4
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-26 03:53:26 +0300

    Add event presenter

---
+++ api/app/Http/Presenters/EventPresenter.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Presenters;
+
+use App\Contracts\PresenterCollectionInterface;
+use App\Entity\Event;
+use Illuminate\Support\Collection;
+
+final class EventPresenter implements PresenterCollectionInterface
+{
+    private EventTypePresenter $eventTypePresenter;
+
+    public function __construct(
+        EventTypePresenter $eventTypePresenter
+    )
+    {
+        $this->eventTypePresenter = $eventTypePresenter;
+    }
+
+    public function present(Event $event): array
+    {
+        return [
+            'id' => $event->id,
+            'invitee_name' => $event->invitee_name,
+            'invitee_email' => $event->invitee_email,
+            'invitee_question' => 'No questions',
+            'start_date' => $event->start_date,
+            'timezone' => $event->timezone,
+            'created_at' => $event->created_at,
+            'event_type' => $this->eventTypePresenter->present($event->eventType),
+        ];
+    }
+
+    public function presentCollection(Collection $eventCollection): array
+    {
+        return $eventCollection->map(function ($event) {
+            return $this->present($event);
+        })->toArray();
+    }
+}
