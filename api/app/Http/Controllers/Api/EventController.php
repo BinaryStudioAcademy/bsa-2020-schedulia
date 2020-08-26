85bf6c032caddf46fc75d5020aafbc8115396eea
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-26 03:54:20 +0300

    Update event controller - add method index (get events paginated)

--- api/app/Http/Controllers/Api/EventController.php
+++ api/app/Http/Controllers/Api/EventController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Actions\Event\AddEventAction;
 use App\Actions\Event\AddEventRequest;
+use App\Actions\Event\GetEventCollectionAction;
+use App\Actions\Event\GetEventCollectionRequest;
+use App\Http\Presenters\EventPresenter;
 use App\Http\Requests\Api\Event\EventRequest;
 use Illuminate\Http\Request;
 
 class EventController extends ApiController
 {
     private AddEventAction $addEventAction;
+    private GetEventCollectionAction $getEventCollectionAction;
+    private $presenter;
 
-    public function __construct(AddEventAction $addEventAction)
+    public function __construct
+    (
+        AddEventAction $addEventAction,
+        GetEventCollectionAction $getEventCollectionAction,
+        EventPresenter $eventPresenter
+    )
     {
         $this->addEventAction = $addEventAction;
+        $this->getEventCollectionAction = $getEventCollectionAction;
+        $this->presenter = $eventPresenter;
     }
 
     public function store(EventRequest $request)
@@ -33,6 +45,20 @@ class EventController extends ApiController
 
     public function index(Request $request)
     {
-        //
+        $response = $this->getEventCollectionAction->execute(
+            new GetEventCollectionRequest(
+                $request->query('startDate'),
+                $request->query('endDate'),
+                (int)$request->query('page'),
+                (int)$request->query('perPage'),
+                $request->query('sort'),
+                $request->query('direction')
+            )
+        );
+
+        return $this->createPaginatedResponse(
+            $response->getPaginator(),
+            $this->presenter
+        );
     }
 }
