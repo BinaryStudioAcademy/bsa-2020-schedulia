215a7b95b449aed420aaa9e631f85f50416380fa
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-27 04:59:53 +0300

    Fix response formatter

--- client/src/store/modules/scheduledEvent/normalizer.js
+++ client/src/store/modules/scheduledEvent/normalizer.js
@@ -4,7 +4,7 @@ export const eventMapper = Event => ({
     id: Event.id,
     name: Event.invitee_name,
     email: Event.invitee_email,
-    questions: Event.invitee_questions,
+    question: Event.invitee_question,
     startDate: Event.start_date,
     startTime: Event.start_time,
     timezone: Event.timezone,
