9499f2c0395996d7db926ce957c22fa32a2c49d0
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-27 16:43:27 +0300

    Fix user factory

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -19,9 +19,11 @@ use Illuminate\Support\Facades\Hash;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $email = $faker->unique()->safeEmail;
+
     return [
         'name' => $faker->name,
-        'email' => $faker->email,
+        'email' => $email,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
