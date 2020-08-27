d99524a2f095d913cded251ebea0368597b4104d
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-27 10:35:07 +0300

    Delete oldest file

--- client/src/services/scheduled-event/scheduledEventsService.js
+++ client/src/services/scheduled-event/scheduledEventsService.js
@@ -1,5 +1,3 @@
-import responseFilterScheduledEventsTypes from './responseFilterScheduledEventsTypes.json';
-import responseFilterScheduledEventsTypesSearch from './responseFilterScheduledEventsTypesSearch.json';
 import requestService from '../requestService';
 
 const scheduledEventService = {
@@ -20,14 +18,6 @@ const scheduledEventService = {
             endDate,
         });
         return response?.data?.data;
-    },
-
-    async getFilterScheduledEventsTypes(eventTypesSearch) {
-        if (eventTypesSearch) {
-            return responseFilterScheduledEventsTypesSearch;
-        } else {
-            return responseFilterScheduledEventsTypes;
-        }
     }
 };
 
