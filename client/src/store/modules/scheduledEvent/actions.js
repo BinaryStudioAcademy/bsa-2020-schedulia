1d8b7a230a3ade0b31cd7af69d7d4eea016f0f38
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-27 22:49:52 +0300

    Add loader

--- client/src/store/modules/scheduledEvent/actions.js
+++ client/src/store/modules/scheduledEvent/actions.js
@@ -2,16 +2,29 @@ import * as actions from './types/actions';
 import * as mutations from './types/mutations';
 import scheduledEventService from '@/services/scheduled-event/scheduledEventsService';
 import { SET_ERROR_NOTIFICATION } from '@/store/modules/notification/types/actions';
+import * as loaderMutations from '@/store/modules/loader/types/mutations';
 
 export default {
     [actions.SET_SCHEDULED_EVENT_FILTER_VIEW]: async (
         { commit },
         scheduledEventFilterView
     ) => {
-        commit(
-            mutations.SET_SCHEDULED_EVENT_FILTER_VIEW,
-            scheduledEventFilterView
-        );
+        commit('loader/' + loaderMutations.SET_LOADING, true, { root: true });
+
+        try {
+            commit(
+                    mutations.SET_SCHEDULED_EVENT_FILTER_VIEW,
+                    scheduledEventFilterView
+            );
+
+            commit('loader/' + loaderMutations.SET_LOADING, false, {root: true});
+        } catch (error) {
+            commit(
+                    SET_ERROR_NOTIFICATION,
+                    error?.response?.data?.message || error.message
+            );
+            commit('loader/' + loaderMutations.SET_LOADING, false, {root: true});
+        }
     },
 
     [actions.SET_SCHEDULED_EVENTS]: async (
@@ -25,6 +38,8 @@ export default {
             endDate = ''
         }
     ) => {
+        commit('loader/' + loaderMutations.SET_LOADING, true, { root: true });
+
         try {
             const events = await scheduledEventService.getScheduledEvents(
                 page,
@@ -41,11 +56,13 @@ export default {
 
             commit(mutations.SET_SCHEDULED_EVENTS_PAGINATION, events.meta);
             commit(mutations.SET_SCHEDULED_EVENTS, events.data);
+            commit('loader/' + loaderMutations.SET_LOADING, false, {root: true});
         } catch (error) {
             commit(
                 SET_ERROR_NOTIFICATION,
                 error?.response?.data?.message || error.message
             );
+            commit('loader/' + loaderMutations.SET_LOADING, false, {root: true});
         }
     }
 };
