03e2e8472e77eff27f75eb267b45ee46a67d590b
dmitriy-sanders <dimauuvin@gmail.com>
2020-08-27 23:50:33 +0300

    Fix validation rules

--- api/app/Http/Requests/Api/CustomField/CustomFieldRequest.php
+++ api/app/Http/Requests/Api/CustomField/CustomFieldRequest.php
@@ -4,24 +4,21 @@ declare(strict_types=1);
 
 namespace App\Http\Requests\Api\CustomField;
 
-use App\Http\Requests\ApiFormRequest;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-final class CustomFieldRequest extends ApiFormRequest
+final class CustomFieldRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'custom_fields' => [
-                '*' => [
-                    'name' => 'required|string',
-                    'type' => [
-                        'required',
-                        'string',
-                        Rule::in(CustomFieldTypes::getAllTypes())
-                    ]
-                ]
-            ]
+            'custom_fields' => 'array',
+            'custom_fields.*.type' => [
+                'required',
+                'string',
+                Rule::in(CustomFieldTypes::getAllTypes())
+            ],
+            'custom_fields.*.name' => 'required|string'
         ];
     }
 }
