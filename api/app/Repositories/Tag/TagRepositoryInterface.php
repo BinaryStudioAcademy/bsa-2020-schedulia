b088617f4aea4edf61fbb6105ac3b157c13d54d0
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-31 17:07:20 +0300

    Create add teg action, route, controller, request, response

--- api/app/Repositories/Tag/TagRepositoryInterface.php
+++ api/app/Repositories/Tag/TagRepositoryInterface.php
@@ -2,9 +2,11 @@
 
 namespace App\Repositories\Tag;
 
+use App\Entity\Tag;
 use Illuminate\Support\Collection;
 
 interface TagRepositoryInterface
 {
+    public function save(Tag $tag): Tag;
     public function findByCriteria(array $criteria): Collection;
 }
