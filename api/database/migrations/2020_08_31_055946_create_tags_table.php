a88fc63bebc8d928a384ce88dfb9156814459798
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-31 09:02:44 +0300

    Add migration

---
+++ api/database/migrations/2020_08_31_055946_create_tags_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('event_type_id');
+            $table->timestamps();
+
+            $table
+                ->foreign('event_type_id')
+                ->references('id')
+                ->on('event_types')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
