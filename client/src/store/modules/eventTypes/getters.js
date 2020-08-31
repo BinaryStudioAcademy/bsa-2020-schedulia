58a5676b5ab85c3c94a1de8eab4c1617fad39210
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-08-31 18:53:55 +0300

    Add event types tags getter

--- client/src/store/modules/eventTypes/getters.js
+++ client/src/store/modules/eventTypes/getters.js
@@ -7,5 +7,9 @@ export default {
         }),
     [getters.GET_EVENT_TYPES_BY_NICKNAME]: state => state.eventTypesByNickname,
     [getters.GET_CUSTOM_FIELDS]: state => state.customFields,
-    [getters.GET_CUSTOM_FIELD_BY_ID]: state => id => state.customFields[id]
+    [getters.GET_CUSTOM_FIELD_BY_ID]: state => id => state.customFields[id],
+    [getters.GET_ALL_EVENT_TYPES_TAGS]: state =>
+            Object.values(state.eventTypesTags).sort((a, b) => {
+                return a.id - b.id;
+            }),
 };
