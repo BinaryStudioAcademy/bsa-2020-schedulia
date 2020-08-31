8f10f95f613611b8cfec4e14857b283f0e27315c
Serhii <sergiykundys@gmail.com>
2020-08-31 21:05:39 +0300

    ran linter

--- client/src/store/modules/eventType/mutations.js
+++ client/src/store/modules/eventType/mutations.js
@@ -1,43 +1,43 @@
-import * as mutations from "./types/mutations";
-import { eventTypeMapper } from "@/store/modules/eventType/normalizer";
-import _ from "lodash";
+import * as mutations from './types/mutations';
+import { eventTypeMapper } from '@/store/modules/eventType/normalizer';
+import _ from 'lodash';
 
 export default {
-  [mutations.SET_EVENT_TYPES]: (state, eventTypes) => {
-    const eventTypeById = {
-      ...state.eventTypes.eventTypeById,
-      ...eventTypes.reduce(
-        (prev, eventType) => ({
-          ...prev,
-          [eventType.id]: eventTypeMapper(eventType),
-        }),
-        {}
-      ),
-    };
+    [mutations.SET_EVENT_TYPES]: (state, eventTypes) => {
+        const eventTypeById = {
+            ...state.eventTypes.eventTypeById,
+            ...eventTypes.reduce(
+                (prev, eventType) => ({
+                    ...prev,
+                    [eventType.id]: eventTypeMapper(eventType)
+                }),
+                {}
+            )
+        };
 
-    state.eventTypes = {
-      eventTypeById: eventTypeById,
-      eventTypes: Object.keys(eventTypeById),
-    };
-  },
-  [mutations.ADD_EVENT_TYPE]: (state, eventType) => {
-    state.eventTypes = {
-      ...state.eventTypes,
-      [eventType.id]: eventTypeMapper(eventType),
-    };
-  },
+        state.eventTypes = {
+            eventTypeById: eventTypeById,
+            eventTypes: Object.keys(eventTypeById)
+        };
+    },
+    [mutations.ADD_EVENT_TYPE]: (state, eventType) => {
+        state.eventTypes = {
+            ...state.eventTypes,
+            [eventType.id]: eventTypeMapper(eventType)
+        };
+    },
 
-  [mutations.SET_EVENT_TYPE]: (state, eventType) => {
-    _.assign(state.eventType, _.pick(eventType, _.keys(state.eventType)));
-  },
+    [mutations.SET_EVENT_TYPE]: (state, eventType) => {
+        _.assign(state.eventType, _.pick(eventType, _.keys(state.eventType)));
+    },
 
-  [mutations.DELETE_EVENT_TYPE]: (state, id) => {
-    const eventTypes = { ...state.eventTypes };
-    delete eventTypes[id];
-    state.eventTypes = eventTypes;
-  },
+    [mutations.DELETE_EVENT_TYPE]: (state, id) => {
+        const eventTypes = { ...state.eventTypes };
+        delete eventTypes[id];
+        state.eventTypes = eventTypes;
+    },
 
-  [mutations.SET_PROPERTY]: (state, { property, value }) => {
-    state[property] = value;
-  },
+    [mutations.SET_PROPERTY]: (state, { property, value }) => {
+        state[property] = value;
+    }
 };
--- client/src/store/modules/eventType/types/mutations.js
+++ client/src/store/modules/eventType/types/mutations.js
@@ -1,5 +1,5 @@
-export const SET_EVENT_TYPES = "SET_EVENT_TYPES";
-export const SET_EVENT_TYPE = "SET_EVENT_TYPE";
-export const ADD_EVENT_TYPE = "ADD_EVENT_TYPE";
-export const DELETE_EVENT_TYPE = "DELETE_EVENT_TYPE";
-export const SET_PROPERTY = "SET_PROPERTY";
+export const SET_EVENT_TYPES = 'SET_EVENT_TYPES';
+export const SET_EVENT_TYPE = 'SET_EVENT_TYPE';
+export const ADD_EVENT_TYPE = 'ADD_EVENT_TYPE';
+export const DELETE_EVENT_TYPE = 'DELETE_EVENT_TYPE';
+export const SET_PROPERTY = 'SET_PROPERTY';
