e7f0bcc259a6e5dc02c587a22474c10e9eacc931
Serhii <sergiykundys@gmail.com>
2020-08-31 21:04:20 +0300

    removed unnecessary data

--- client/src/store/modules/eventType/mutations.js
+++ client/src/store/modules/eventType/mutations.js
@@ -1,61 +1,43 @@
-import * as mutations from './types/mutations';
-import { eventTypeMapper } from '@/store/modules/eventType/normalizer';
-import _ from 'lodash';
+import * as mutations from "./types/mutations";
+import { eventTypeMapper } from "@/store/modules/eventType/normalizer";
+import _ from "lodash";
 
 export default {
-    [mutations.SET_EVENT_TYPES]: (state, eventTypes) => {
-        const eventTypeById = {
-            ...state.eventTypes.eventTypeById,
-            ...eventTypes.reduce(
-                (prev, eventType) => ({
-                    ...prev,
-                    [eventType.id]: eventTypeMapper(eventType)
-                }),
-                {}
-            )
-        };
+  [mutations.SET_EVENT_TYPES]: (state, eventTypes) => {
+    const eventTypeById = {
+      ...state.eventTypes.eventTypeById,
+      ...eventTypes.reduce(
+        (prev, eventType) => ({
+          ...prev,
+          [eventType.id]: eventTypeMapper(eventType),
+        }),
+        {}
+      ),
+    };
 
-        state.eventTypes = {
-            eventTypeById: eventTypeById,
-            eventTypes: Object.keys(eventTypeById)
-        };
-    },
-    [mutations.ADD_EVENT_TYPE]: (state, eventType) => {
-        state.eventTypes = {
-            ...state.eventTypes,
-            [eventType.id]: eventTypeMapper(eventType)
-        };
-    },
+    state.eventTypes = {
+      eventTypeById: eventTypeById,
+      eventTypes: Object.keys(eventTypeById),
+    };
+  },
+  [mutations.ADD_EVENT_TYPE]: (state, eventType) => {
+    state.eventTypes = {
+      ...state.eventTypes,
+      [eventType.id]: eventTypeMapper(eventType),
+    };
+  },
 
-    [mutations.SET_EVENT_TYPE]: (state, eventType) => {
-        _.assign(state.eventType, _.pick(eventType, _.keys(state.eventType)));
-    },
+  [mutations.SET_EVENT_TYPE]: (state, eventType) => {
+    _.assign(state.eventType, _.pick(eventType, _.keys(state.eventType)));
+  },
 
-    [mutations.DELETE_EVENT_TYPE]: (state, id) => {
-        const eventTypes = { ...state.eventTypes };
-        delete eventTypes[id];
-        state.eventTypes = eventTypes;
-    },
+  [mutations.DELETE_EVENT_TYPE]: (state, id) => {
+    const eventTypes = { ...state.eventTypes };
+    delete eventTypes[id];
+    state.eventTypes = eventTypes;
+  },
 
-    [mutations.SET_EVENT_TYPE_FORM]: (state, data) => {
-        _.assign(
-            state.eventTypeForm,
-            _.pick(data, _.keys(state.eventTypeForm))
-        );
-    },
-
-    [mutations.SET_EVENT_TYPE_FORM_COLUMN_DISABLE]: (state, data) => {
-        state.eventTypeForm.disabled = data;
-    },
-
-    [mutations.SET_EVENT_TYPE_FORM_LOCATION_TYPE]: (state, data) => {
-        state.eventTypeForm.locationType = data;
-    },
-
-    [mutations.SET_EVENT_TYPE_FORM_LOCATION]: (state, data) => {
-        state.eventTypeForm.location = data;
-    },
-    [mutations.SET_PROPERTY]: (state, { property, value }) => {
-        state[property] = value;
-    }
+  [mutations.SET_PROPERTY]: (state, { property, value }) => {
+    state[property] = value;
+  },
 };
--- client/src/store/modules/eventType/normalizer.js
+++ client/src/store/modules/eventType/normalizer.js
@@ -1,57 +1,59 @@
-export const eventTypeMapper = EventType => ({
-    id: EventType.id,
-    name: EventType.name,
-    description: EventType.description,
-    slug: EventType.slug,
-    color: EventType.color,
-    duration: EventType.duration,
-    disabled: EventType.disabled,
-    timezone: EventType.timezone,
-    owner: userMapper(EventType.owner),
-    availabilities: EventType.availabilities.map(availabilityMapper)
+export const eventTypeMapper = (EventType) => ({
+  id: EventType.id,
+  name: EventType.name,
+  description: EventType.description,
+  location: EventType.location,
+  locationType: EventType.location_type,
+  coordinates: EventType.coordinates,
+  slug: EventType.slug,
+  color: EventType.color,
+  duration: EventType.duration,
+  disabled: EventType.disabled,
+  timezone: EventType.timezone,
+  owner: userMapper(EventType.owner),
+  availabilities: EventType.availabilities.map(availabilityMapper),
 });
 
-export const userMapper = user => ({
-    id: user.id,
-    email: user.email,
-    name: user.name,
-    timezone: user.timezone,
-    nickname: user.nickname
+export const userMapper = (user) => ({
+  id: user.id,
+  email: user.email,
+  name: user.name,
+  timezone: user.timezone,
+  nickname: user.nickname,
 });
 
-export const availabilityMapper = availability => ({
-    type: availability.type,
-    startDate: availability.start_date,
-    endDate: availability.end_date
+export const availabilityMapper = (availability) => ({
+  type: availability.type,
+  startDate: availability.start_date,
+  endDate: availability.end_date,
 });
 
-export const availabilityApiMapper = availability => ({
-    type: availability.type,
-    start_date: availability.startDate,
-    end_date: availability.endDate,
-    start_time: availability?.startTime,
-    end_time: availability?.endTime
+export const availabilityApiMapper = (availability) => ({
+  type: availability.type,
+  start_date: availability.startDate,
+  end_date: availability.endDate,
+  start_time: availability?.startTime,
+  end_time: availability?.endTime,
 });
 
-export const eventTypeFormMapper = eventTypeForm => ({
-    name: eventTypeForm.name,
-    location: eventTypeForm.location,
-    locationType: eventTypeForm.locationType,
-    description: eventTypeForm.description,
-    slug: eventTypeForm.slug,
-    color: eventTypeForm.color,
-    duration: eventTypeForm.duration || eventTypeForm.customDuration,
-    disabled: eventTypeForm.disabled,
-    timezone: eventTypeForm.timezone,
-    availabilities: availabilitiesMapper(eventTypeForm.availabilities)
+export const eventTypeFormMapper = (eventTypeForm) => ({
+  name: eventTypeForm.name,
+  location: eventTypeForm.location,
+  location_type: eventTypeForm.locationType,
+  coordinates: eventTypeForm.coordinates,
+  description: eventTypeForm.description,
+  slug: eventTypeForm.slug,
+  color: eventTypeForm.color,
+  duration: eventTypeForm.duration || eventTypeForm.customDuration,
+  disabled: eventTypeForm.disabled,
+  timezone: eventTypeForm.timezone,
+  availabilities: availabilitiesMapper(eventTypeForm.availabilities),
 });
 
 export const availabilitiesMapper = function(availabilities) {
-    return Object.entries(availabilities).flatMap(availabilities =>
-        availabilities[1]
-            .filter(
-                availability => availability.startDate && availability.endDate
-            )
-            .map(availability => availabilityApiMapper(availability))
-    );
+  return Object.entries(availabilities).flatMap((availabilities) =>
+    availabilities[1]
+      .filter((availability) => availability.startDate && availability.endDate)
+      .map((availability) => availabilityApiMapper(availability))
+  );
 };
