1d8c6911eee2b09afeaa383e12e4665d3196ba90
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-09-01 10:21:05 +0300

    Fix code style

--- client/src/components/scheduled-events/SearchInput.vue
+++ client/src/components/scheduled-events/SearchInput.vue
@@ -1,73 +1,73 @@
 <template>
     <div class="search-input">
         <VTextField
-                hide-details
-                type="text"
-                solo
-                :label="lang.SEARCH"
-                prepend-inner-icon="mdi-magnify"
-                dense
-                :value="searchString"
-                @input="onSearchInput"
-                color="var(--v-customDark-base)"
-                clearable
+            hide-details
+            type="text"
+            solo
+            :label="lang.SEARCH"
+            prepend-inner-icon="mdi-magnify"
+            dense
+            :value="searchString"
+            @input="onSearchInput"
+            color="var(--v-customDark-base)"
+            clearable
         ></VTextField>
     </div>
 </template>
 
 <script>
-    import * as i18nGetters from '@/store/modules/i18n/types/getters';
-    import {mapGetters} from "vuex";
+import * as i18nGetters from '@/store/modules/i18n/types/getters';
+import { mapGetters } from 'vuex';
 
-    export default {
-        name: "SearchInput",
+export default {
+    name: 'SearchInput',
 
-        data: () => ({
-            searchString: '',
-        }),
+    data: () => ({
+        searchString: ''
+    }),
 
-        watch: {
-            $route: 'setSearchInput'
-        },
+    watch: {
+        $route: 'setSearchInput'
+    },
 
-        async mounted() {
-                await this.setSearchInput();
-        },
+    async mounted() {
+        await this.setSearchInput();
+    },
 
-        computed: {
-            ...mapGetters('i18n', {
-                lang: i18nGetters.GET_LANGUAGE_CONSTANTS
-            }),
-        },
+    computed: {
+        ...mapGetters('i18n', {
+            lang: i18nGetters.GET_LANGUAGE_CONSTANTS
+        })
+    },
 
-        methods: {
-            onSearchInput(searchString) {
-                this.$router.push({
-                    name: 'Past',
-                    query: {
-                        event_types: this.$route.query.event_types,
-                        event_emails: this.$route.query.event_emails,
-                        event_status: this.$route.query.event_status,
-                        tags: this.$route.query.tags,
-                        search: searchString
-                    }
-                });
-            },
-
-            setSearchInput() {
-                if (this.$route.query.search) {
-                    this.searchString = this.$route.query.search;
-                } else {
-                    this.searchString = '';
+    methods: {
+        onSearchInput(searchString) {
+            this.$router.push({
+                name: 'Past',
+                query: {
+                    event_types: this.$route.query.event_types,
+                    event_emails: this.$route.query.event_emails,
+                    event_status: this.$route.query.event_status,
+                    tags: this.$route.query.tags,
+                    search: searchString
                 }
+            });
+        },
+
+        setSearchInput() {
+            if (this.$route.query.search) {
+                this.searchString = this.$route.query.search;
+            } else {
+                this.searchString = '';
             }
         }
-    };
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-    .search-input {
-        width: 150px;
-        display: inline-flex;
-    }
+.search-input {
+    width: 150px;
+    display: inline-flex;
+}
 </style>
