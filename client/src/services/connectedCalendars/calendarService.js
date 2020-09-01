fdde4269c1d72f471f2de1d546d50f6cad7482eb
Mike Maschin <aussie.m.mike@gmail.com>
2020-09-01 12:39:28 +0300

    social-accounts to constant

--- client/src/services/connectedCalendars/calendarService.js
+++ client/src/services/connectedCalendars/calendarService.js
@@ -1,13 +1,15 @@
 import requestService from '@/services/requestService';
 
+const SOCIAL_ACCOUNTS_ENDPOINT = '/social-accounts';
+
 export default {
     async connect(calendar) {
-        return await requestService.get(`/social-accounts/${calendar}/oauth`);
+        return await requestService.get(`${SOCIAL_ACCOUNTS_ENDPOINT}/${calendar}/oauth`);
     },
 
     async disconnect(provider) {
         return await requestService.delete(
-            `/social-accounts/calendar/${provider}`
+            `${SOCIAL_ACCOUNTS_ENDPOINT}/calendar/${provider}`
         );
     }
 };
