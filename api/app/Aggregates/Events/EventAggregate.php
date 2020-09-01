4ffa8e13344b5979d5396c99f86b4ec8b68d0b2e
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-09-02 00:17:06 +0300

    Change event aggregate add fields

--- api/app/Aggregates/Events/EventAggregate.php
+++ api/app/Aggregates/Events/EventAggregate.php
@@ -5,21 +5,31 @@ declare(strict_types=1);
 namespace App\Aggregates\Events;
 
 use App\Entity\Event;
-use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 
 class EventAggregate
 {
     private int $id;
     private string $inviteeEmail;
-    private Carbon $startDate;
-    private string $eventType;
+    private string $inviteeName;
+    private string $timezone;
+    private string $status;
+    private string $startDate;
+    private int $eventTypeId;
+    private int $eventTypeOwnerId;
+    private ?Collection $eventTypeTagsId;
 
     public function __construct(Event $event)
     {
         $this->id = $event->id;
         $this->inviteeEmail = $event->invitee_email;
+        $this->inviteeName = $event->invitee_name;
+        $this->timezone = $event->timezone;
+        $this->status = $event->status;
         $this->startDate = $event->start_date;
-        $this->eventType = $event->event_type_id;
+        $this->eventTypeId = $event->event_type_id;
+        $this->eventTypeOwnerId = $event->eventType->owner_id;
+        $this->eventTypeTagsId = $event->eventType->tags;
     }
 
     public function getId(): int
@@ -32,8 +42,13 @@ class EventAggregate
         return [
             'id' => $this->id,
             'invitee_email' => $this->inviteeEmail,
+            'invitee_name' => $this->inviteeName,
+            'timezone' => $this->timezone,
+            'status' => $this->status,
             'start_date' => $this->startDate,
-            'event_type' => $this->eventType,
+            'event_type_id' => $this->eventTypeId,
+            'event_type_owner_id' => $this->eventTypeOwnerId,
+            'event_type_tags' => $this->eventTypeTagsId
         ];
     }
 }
