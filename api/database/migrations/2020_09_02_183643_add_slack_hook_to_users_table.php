d96f7780924e142b78e73efa2de9329db74788cb
dmitriy-sanders <dimauuvin@gmail.com>
2020-09-02 21:40:58 +0300

    Add migration for users

---
+++ api/database/migrations/2020_09_02_183643_add_slack_hook_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSlackHookToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('slack_webhook')->nullable();
+            $table->string('slack_channel')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('slack_webhook');
+            $table->dropColumn('slack_channel');
+        });
+    }
+}
