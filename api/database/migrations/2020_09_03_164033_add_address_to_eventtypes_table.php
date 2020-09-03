d736850105b71dc34d0075e7ea9a0a5a865b0ec1
mv-v-7 <69146987+mv-v-7@users.noreply.github.com>
2020-09-03 21:51:15 +0300

    fixes

--- api/database/migrations/2020_09_03_164033_add_address_to_eventtypes_table.php
+++ api/database/migrations/2020_09_03_164033_add_address_to_eventtypes_table.php
@@ -14,7 +14,7 @@ class AddAddressToEventtypesTable extends Migration
     public function up()
     {
         Schema::table('event_types', function (Blueprint $table) {
-            $table->longText('address')->nullable();
+            $table->string('address')->nullable();
         });
     }
 
