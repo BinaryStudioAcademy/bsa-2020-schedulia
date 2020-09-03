f48db24facd2b97cebc72520ded6054cd52ceca5
Yevhen Turovets <yevhen.turovets@gmail.com>
2020-09-03 10:18:26 +0300

    Add search method to Interface

--- api/app/Repositories/ElasticSearch/Events/EventAggregateRepositoryInterface.php
+++ api/app/Repositories/ElasticSearch/Events/EventAggregateRepositoryInterface.php
@@ -3,8 +3,15 @@
 namespace App\Repositories\ElasticSearch\Events;
 
 use App\Aggregates\Events\EventAggregate;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 interface EventAggregateRepositoryInterface
 {
     public function save(EventAggregate $eventAggregate): EventAggregate;
+    public function search (
+        array $criteria,
+        int $page,
+        int $perPage,
+        string $sort,
+        string $direction): LengthAwarePaginator;
 }
