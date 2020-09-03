a28c1ba33c9bb7d6acc9395a3084950b8b6ba601
Mike Maschin <aussie.m.mike@gmail.com>
2020-09-03 12:00:27 +0300

    resolve conflicts in event repository

--- api/app/Repositories/Event/EventRepository.php
+++ api/app/Repositories/Event/EventRepository.php
@@ -64,8 +64,7 @@ final class EventRepository extends BaseRepository implements EventRepositoryInt
             ->distinct()
             ->get();
     }
-
-
+    
     public function getById(int $id): ?Event
     {
         return Event::find($id);
