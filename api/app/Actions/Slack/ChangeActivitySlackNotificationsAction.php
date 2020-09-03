8f00247d9b64fce55e9a04e735853915cf0585b9
dmitriy-sanders <dimauuvin@gmail.com>
2020-09-03 15:24:53 +0300

    Update actions

--- api/app/Actions/Slack/ChangeActivitySlackNotificationsAction.php
+++ api/app/Actions/Slack/ChangeActivitySlackNotificationsAction.php
@@ -4,14 +4,24 @@ declare(strict_types=1);
 
 namespace App\Actions\Slack;
 
+use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
 
 final class ChangeActivitySlackNotificationsAction
 {
-    public function execute()
-    {
-        $user = Auth::user();
+    private UserRepositoryInterface $userRepository;
 
-        $user->slack_active = !$user->slack_active;
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function execute(ChangeActivitySlackNotificationsRequest $request)
+    {
+        $user = $this->userRepository->getById(Auth::id());
+
+        $user->slack_active = $request->getActivity();
+
+        $this->userRepository->save($user);
     }
 }
