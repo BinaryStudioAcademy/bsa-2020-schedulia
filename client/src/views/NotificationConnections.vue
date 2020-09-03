c5c6501cb4d3593ace27a2f8fde46a383b190e47
dmitriy-sanders <dimauuvin@gmail.com>
2020-09-03 20:47:18 +0300

    Fix localization + add Chatito component

--- client/src/views/NotificationConnections.vue
+++ client/src/views/NotificationConnections.vue
@@ -1,22 +1,30 @@
 <template>
     <GeneralLayout>
         <template v-slot:title>
-            Notification Connections
+            {{ lang.NOTIFICATION_CONNECTIONS }}
         </template>
-        <SlackNotificationConnection />
+        <div class="container">
+            <div class="row-flex flex-wrap flex-row d-flex">
+                <SlackNotificationConnection />
+                <ChatitoNotificationConnection />
+            </div>
+        </div>
     </GeneralLayout>
 </template>
 
 <script>
 import GeneralLayout from '@/components/common/GeneralLayout/GeneralLayout';
 import SlackNotificationConnection from '@/components/notification-connections/slack/SlackNotificationConnection';
+import ChatitoNotificationConnection from '@/components/notification-connections/chatito/ChatitoNotificationConnection';
 import { mapGetters } from 'vuex';
 import * as getters from '@/store/modules/auth/types/getters';
+import * as langGetters from '@/store/modules/i18n/types/getters';
 export default {
     name: 'NotificationConnections',
     components: {
         GeneralLayout,
-        SlackNotificationConnection
+        SlackNotificationConnection,
+        ChatitoNotificationConnection
     },
     data: () => ({
         slackIsActive: false
@@ -24,6 +32,9 @@ export default {
     computed: {
         ...mapGetters('auth', {
             user: getters.GET_LOGGED_USER
+        }),
+        ...mapGetters('i18n', {
+            lang: langGetters.GET_LANGUAGE_CONSTANTS
         })
     },
     async mounted() {
