3afdab6434ab4a1d46063f9fb3b2679eb8739f97
dmitriy-sanders <dimauuvin@gmail.com>
2020-09-05 15:45:51 +0300

    Add message for Chatito

---
+++ api/app/Notifications/Chatito/ChatitoMessage.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Notifications\Chatito;
+
+
+use App\Entity\Event;
+use Illuminate\Support\Facades\Http;
+
+class ChatitoMessage
+{
+    public function __construct(Event $event)
+    {
+        $this->event = $event;
+        $this->eventType = $event->eventType;
+        $this->user = $event->eventType->owner;
+
+        $this->sendMessageToChatito();
+    }
+
+    public function sendMessageToChatito()
+    {
+        Http::post(env('CHATITO_ENDPOINT'), [
+            'data' => [
+                'message' => $this->generateMessage()
+            ],
+            'users' => [
+                $this->user->email,
+                $this->event->invitee_email,
+            ],
+            'workspace' => $this->eventType->chatito_workspace
+        ]);
+    }
+}
